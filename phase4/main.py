6d850bbd77651e3cf7a163a632801fb9c7887540
Emiya2098212383 <52161145+Emiya2098212383@users.noreply.github.com>
2019-08-07 13:32:10 +0800

    ？！

---
+++ phase4/main.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+# Filename : 01-string.py
+# author by : Emiya
+
+from person_chinese import Person,Chinese
+
+print('--------cutting line-----------')
+man = Person()
+man.talk()
+
+print('--------cutting line-----------')
+Chinese_man = Chinese()
+Chinese_man.talk()
+Chinese_man.walk()
---
+++ phase4/person_chinese.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+# Filename : 01-string.py
+# author by : Emiya
+
+class Person():
+    def talk(self):
+        print('people is talking......')
+        return
+
+class Chinese(Person):
+    def walk(self):
+        print('A Chinese is walking......')
+        return
+
+        
